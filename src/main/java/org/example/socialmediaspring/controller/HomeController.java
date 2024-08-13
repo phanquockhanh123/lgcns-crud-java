64ab43d1a5264bbc2192c312128e76de3cdc525f
63200332 <khanhphanquoc68@gmail.com>
2024-08-13 00:01:03 -0700

    push code handle internationalize

--- src/main/java/org/example/socialmediaspring/controller/HomeController.java
+++ src/main/java/org/example/socialmediaspring/controller/HomeController.java
@@ -1,16 +1,37 @@
 package org.example.socialmediaspring.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping("api/v1")
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final MessageSource messageSource;
+
     @RequestMapping("/hello")
     public String hello() {
         return "Hello spring boot";
     }
+
+
+    @GetMapping(path = "/hello-word-internationlized")
+    public String convertLang() {
+        Locale locale = LocaleContextHolder.getLocale();
+
+        return  messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+    }
+
+    //
 
 
 }
